1d825427cc876b0686894a8814b9fc562daf5922
Manuela Gomez <mmanu.gomez@gmail.com>
2020-01-30 09:15:28 -0500

    [WIP] App - Added pagination style with ant design

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
-import { Menu } from 'antd';
+import { Menu, Input, Pagination } from 'antd';
+const { Search } = Input;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      current: 'Main',
+      menu: 'Main',
+      currentPage: '1',
     };
+    this.handleMenuClick = this.handleMenuClick.bind(this);
+
   }
 
-  handleClick(e) {
+  handleMenuClick(e) {
     console.log('click', e);
     this.setState({
-      current: e.key,
+      menu: e.key,
     });
   }
 
+  onPageChange(page) {
+    console.log(page);
+    this.setState({
+      currentPage: page,
+    });
+  }
+
+  // user the Server.search method to get results
+
   render() {
+    // our server is imported as props in the index file
+    const { Server } = this.props;
     return (
-      <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
-        <Menu.Item key="main">
-          Main
-        </Menu.Item>
-      </Menu>
+      <div>
+        <Menu onClick={this.handleMenuClick} selectedKeys={[this.state.menu]} mode="horizontal">
+          <Menu.Item key="main">
+            Main
+          </Menu.Item>
+          <Menu.Item key="search" disables="true">
+            {/* TO DO connect search input to state and to url */}
+            <Search placeholder="input search text" onSearch={value => console.log(value)} enterButton />
+          </Menu.Item>
+        </Menu>
+        <Pagination current={this.state.currentPage} onChange={this.onPageChange} total={20} />
+      </div>
     );
   }
 }
