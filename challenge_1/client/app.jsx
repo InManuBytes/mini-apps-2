2ba012bc7eb6e9b00568f97a61b6d355712813b1
Manuela Gomez <mmanu.gomez@gmail.com>
2020-01-30 08:09:03 -0500

    [WIP] App renders using ant design menu

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
+import { Menu } from 'antd';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      current: 'Main',
     };
+  }
+
+  handleClick(e) {
+    console.log('click', e);
+    this.setState({
+      current: e.key,
+    });
   }
 
   render() {
     return (
-      <div>
-        This is a React app compiled with webpack!
-      </div>
+      <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+        <Menu.Item key="main">
+          Main
+        </Menu.Item>
+      </Menu>
     );
   }
 }
