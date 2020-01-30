0dd3fc84a2b8bb3904e327f92071042887584cd8
Manuela Gomez <mmanu.gomez@gmail.com>
2020-01-30 08:05:22 -0500

    [Webpack] using css and style loaders

--- challenge_1/webpack.config.js
+++ challenge_1/webpack.config.js
@@ -10,6 +10,13 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+        ]
       }
     ]
   },
