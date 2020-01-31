f1e432e07a66e8a695e18b0a07a326c9c4f35b9b
Manuela Gomez <mmanu.gomez@gmail.com>
2020-01-31 18:01:14 -0500

    [Server] using right endpoint and searchURL

--- challenge_1/client/server.js
+++ challenge_1/client/server.js
@@ -1,5 +1,5 @@
 const Server = {
-  address: 'http://localhost:3000/',
+  address: 'http://localhost:3000/events/',
   // server-side pagination: GET /endpoint?_page=7 and
   // full-text search GET /endpoint?q=SEARCH_TERM
   // so we should be able to do GET /events?q=SEARCH_TERM&_page=PAGE_NUMBER&_limit=PER_PAGE_RESULTS
@@ -12,17 +12,19 @@ const Server = {
   },
   // TODO allow user to change results per page
   // QUESTION: does json server return the total number of results + pages?
+  // QUESTION: why does it say it can be converted to an async function?
   search: (searchTerm, pageNumber) => {
     // create query-string
     const pageString = Server.pagination(pageNumber);
     const searchString = Server.fullTextSearch(searchTerm);
     // TO DO create a more dynamically built query-string that goes through the arguments?
     const queryString = `?${searchString}&${pageString}&_limit=20`;
-    const searchURL = `Server.address${queryString}`;
+    const searchURL = `${Server.address}${queryString}`;
     return fetch(searchURL)
       .then(result => {
         console.log(result);
       })
+      // TO DO figure out a better way to deal with errors
       .catch(err => console.log(err));
   }
 
