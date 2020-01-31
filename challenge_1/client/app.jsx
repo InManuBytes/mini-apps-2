89b094d1fe20c9fa30a58d24f7b2f87fa4f1a22c
Manuela Gomez <mmanu.gomez@gmail.com>
2020-01-31 00:42:49 -0500

    [WIP] Added layout, search bar, and styling for content and pagination

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
-import { Menu, Input, Pagination } from 'antd';
+import { Input, Pagination, Layout, Row, Col, Divider } from 'antd';
+const { Header, Content, Footer } = Layout;
 const { Search } = Input;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      menu: 'Main',
-      currentPage: '1',
+      currentPage: 1, // default page to 1
     };
-    this.handleMenuClick = this.handleMenuClick.bind(this);
-
-  }
-
-  handleMenuClick(e) {
-    console.log('click', e);
-    this.setState({
-      menu: e.key,
-    });
+    this.onPageChange = this.onPageChange.bind(this);
   }
 
   onPageChange(page) {
@@ -27,23 +19,46 @@ class App extends Component {
     });
   }
 
-  // user the Server.search method to get results
+  // TODO use the Server.search method to get results
+
 
   render() {
     // our server is imported as props in the index file
     const { Server } = this.props;
     return (
       <div>
-        <Menu onClick={this.handleMenuClick} selectedKeys={[this.state.menu]} mode="horizontal">
-          <Menu.Item key="main">
-            Main
-          </Menu.Item>
-          <Menu.Item key="search" disables="true">
+        <Layout>
+          <Header style={{ color: 'white', position: 'fixed', zIndex: 1, width: '100%' }}>
+            <Row>
+              <Col xs={0} sm={10} md={8} lg={8} xl={6}>
+                Historical Events Finder
+              </Col>
+              <Col xs={0} sm={2} md={8} lg={8} xl={10}>
+              </Col>
+              <Col xs={24} sm={12} md={8} lg={8} xl={8}>
+                <Search placeholder="input key word" onSearch={value => console.log(value)} />
+              </Col>
+            </Row>
+          </Header>
+          <Content style={{ padding: '0 50px', marginTop: 64 }}>
             {/* TO DO connect search input to state and to url */}
-            <Search placeholder="input search text" onSearch={value => console.log(value)} enterButton />
-          </Menu.Item>
-        </Menu>
-        <Pagination current={this.state.currentPage} onChange={this.onPageChange} total={20} />
+            {/* TO DO when there is nothing being searched pagination should not appear, maybe display a message? */}
+            <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>
+              Content
+            </div>
+            <div style={{ background: '#fff', padding: 24}} >
+              <Divider />
+              <Pagination
+                size="small"
+                current={this.state.currentPage}
+                onChange={this.onPageChange}
+                total={20}
+                showTotal={(total, range) => `${range[0]}-${range[1]} of ${total} items`}
+              />
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Manu ©2020</Footer>
+        </Layout>
       </div>
     );
   }
