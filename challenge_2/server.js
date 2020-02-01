cd089ce15d19737bbf754c7cf80c1597d026ac62
Manuela Gomez <mmanu.gomez@gmail.com>
2020-02-01 09:53:04 -0500

    Webpack, babel and react setup with node server for bundled files

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.listen(PORT, () => {
+  console.log(`Event module listening on port ${PORT}`);
+});
 
