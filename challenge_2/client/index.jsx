e182f1e7e3e28c74701f2f663da401bd226e5524
Manuela Gomez <mmanu.gomez@gmail.com>
2020-02-01 18:09:39 -0500

    Connected through server with props

--- challenge_2/client/index.jsx
+++ challenge_2/client/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import Server from './server';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App server={Server} />, document.getElementById('app'));
