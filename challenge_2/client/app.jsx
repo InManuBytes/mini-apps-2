966a437184475283b05377509ae0b42c06a9508b
Manuela Gomez <mmanu.gomez@gmail.com>
2020-02-01 18:09:11 -0500

    Linegraph renders stock prices data passed down as props

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
+import './styles.css';
+import Layout from './layout';
+import LineGraph from './LineGraph';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      prices: {},
+      stock: 'bpi',
     };
   }
 
+  componentDidMount() {
+    const { server }  = this.props;
+    // const { stock } = this.state;
+    return server.getPrices()
+      .then(prices => {
+        this.setState( { prices: prices.bpi })
+      })
+  }
+
   render() {
+    const { stock, prices } = this.state;
     return (
-      <div>
-        This is a React app compiled with webpack!
-      </div>
+      <Layout>
+        <h2 className="leading-none font-bold text-2xl xs:text-2x1 md:text-5xl lg:6x1 uppercase">
+          {stock}
+        </h2>
+        <LineGraph prices={prices} />
+      </Layout>
     );
   }
 }
