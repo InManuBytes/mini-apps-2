2c00b83c288e0335b132259739cdb672fa9b9ced
Manuela Gomez <mmanu.gomez@gmail.com>
2020-02-01 11:50:09 -0500

    Using moment to format date if it's not year only

--- challenge_1/client/utils.js
+++ challenge_1/client/utils.js
@@ -1,20 +1,18 @@
+import moment from 'moment';
+
 const utils = {
   // returns a date in the right format
   formatDate: (date) => {
-    // test if date has any "/" or not to see if it's year only or YYYY/MM/DD
-    const isYearOnly = /[0-9]+(?!\\)/.test(date);
     // check if it's BCE or CE
     const isBCE = /^-/.test(date);
-
-    if (isYearOnly && isBCE) {
-      var year = date.match(/[0-9]+/);
-      return `${year} B.C.E.`;
-    } else if (isYearOnly) {
-      return `${date} C.E`;
-    } else {
-      // convert date to a readable format and check to see if adds BCE or CE
-    }
-
+    const unit = isBCE ? ' BCE' : '';
+    var formattedDate = '';
+    const absDate = date.replace(/^-/, '');
+    const isNotYearOnly = /[0-9]+(?=\/)/.test(date);
+    const newDate = isNotYearOnly ? moment(absDate).format('LL').replace(/ 0/,' ') : absDate;
+    formattedDate += newDate;
+    formattedDate += unit;
+    return formattedDate;
   }
 }
 
