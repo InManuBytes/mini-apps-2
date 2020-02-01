4d7c7135098c7ceea7b08eee05631d65fe840c2a
Manuela Gomez <mmanu.gomez@gmail.com>
2020-02-01 13:45:03 -0500

    Added promises, simple http client, and moved server scripts to own folder

--- challenge_2/server/index.js
+++ challenge_2/server/index.js
@@ -12,6 +12,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, './public')));
 
+// https://api.coindesk.com/v1/bpi/currentprice.json
+// https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05
+// Start building your app using a single, fixed currency symbol,
+// Bitcoin (BTC), and a fixed date range of your choosing.
+// Use a time-series chart and show closing prices only.
+
+app.get('/prices', function (req, res, next) {
+
+});
+
 app.listen(PORT, () => {
   console.log(`Event module listening on port ${PORT}`);
 });
