c08322ea23d577879ad92e27e3896a166135ad46
Manuela Gomez <mmanu.gomez@gmail.com>
2020-02-26 23:04:50 -0500

    General layout for the app with a logo and title

---
+++ challenge_3/client/layout.jsx
@@ -0,0 +1,34 @@
+// make the layout here
+import React from 'react';
+
+const Layout = ({children}) => {
+  return (
+  // layout using hero http://bulma.io/documenatation/layout/hero
+    <section className="hero is-success is-fullheight">
+      {/* Hero head will stick at the top */}
+      <div className="hero-head">
+        <nav className="navbar">
+          <div className="container">
+            <div className="navbar-brand">
+              <img src="bowling-logo-vector.png" alt="Logo"></img>
+            </div>
+            <span className="navbar-item">
+              <h1 className="title">
+                   Bowling Score Keeper
+              </h1>
+            </span>
+          </div>
+        </nav>
+        {/* body */}
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            {/* content */}
+          </div>
+        </div>
+        {/* footer */}
+      </div>
+    </section>
+  );
+};
+
+export default Layout;
