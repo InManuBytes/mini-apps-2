90ba135376814f13e30864b828076e801face6c2
Manuela Gomez <mmanu.gomez@gmail.com>
2020-02-28 00:25:34 -0500

    [WIP] refactored to class component

--- challenge_3/client/components/selector.jsx
+++ challenge_3/client/components/selector.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Selector = ({options}) => {
-  return (
-    <tr>
-      <th>Knocked</th>
-      <td>
-        <div className="select">
-          <select>
-            {options.map(option => {
-              return <option key={option}>{option}</option>
-            })}
-          </select>
-        </div>
-      </td>
-      <td></td>
-    </tr>
-  );
+class Selector extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    const {options} = this.props;
+    return (
+      <tr>
+        <th>Knocked</th>
+        <td>
+          <div className="select">
+            <select>
+              {options.map(option => {
+                return <option key={option}>{option}</option>
+              })}
+            </select>
+          </div>
+        </td>
+        <td></td>
+      </tr>
+    );
+  }
 }
 
 export default Selector;
